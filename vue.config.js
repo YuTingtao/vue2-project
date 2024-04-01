3407bfd0672431da31437a67fdcad9cd706f4c6f
yutingtao <735126858@qq.com>
2024-04-01 17:18:39 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
           enabled: false,
         },
         pngquant: {
-          quality: [0.65, 0.90],
+          quality: [0.8, 0.9],
           speed: 4
         },
         gifsicle: {
